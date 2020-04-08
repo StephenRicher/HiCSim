6bb9d30110688c329ea9123bc902facf3af5b6e7
Stephen Richer <sr467@bath.ac.uk>
2020-04-08 09:52:15 +0100

    Enable computation of median.

--- workflow/scripts/average_matrices.py
+++ workflow/scripts/average_matrices.py
@@ -13,7 +13,7 @@ def main():
     __version__ = '1.0.0'
 
     parser = pct.make_parser(verbose=True, version=__version__)
-    parser.set_defaults(function=plot_heatmap)
+    parser.set_defaults(function=average_heatmap)
 
     parser.add_argument(
         'matrices', nargs='+',
@@ -21,16 +21,34 @@ def main():
     parser.add_argument(
         '-o', '--out', default='contacts-summed.txt',
         help='Summed contact matrix (default: %(default)s)')
+    parser.add_argument(
+        '--method', default='median', choices=['mean', 'median'],
+        help='Method to compute average of matrices (default: %(default)s)')
 
     return (pct.execute(parser))
 
 
-def plot_heatmap(matrices: List, out: str) -> None:
+def compute_median(matrices: List):
+    # Stack matrices along a third dimension and compute median
+    matrices = np.dstack([np.loadtxt(matrix) for matrix in matrices])
 
+    return np.median(matrices, axis = 2)
+
+
+def compute_mean(matrices: List):
     summed_matrix = 0
     for matrix in matrices:
         summed_matrix += np.loadtxt(matrix)
-    average_matrix = summed_matrix / len(matrices)
+
+    return summed_matrix / len(matrices)
+
+
+def average_heatmap(matrices: List, out: str, method: str) -> None:
+
+    if method == 'median':
+        average_matrix = compute_median(matrices)
+    else:
+        average_matrix = compute_mean(matrices)
 
     np.savetxt(out, average_matrix)
 
