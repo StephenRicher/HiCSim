4692ccf195b8fd527dff8efbe0e9b4ecbfc189ec
Stephen Richer <sr467@bath.ac.uk>
2020-04-28 00:28:10 +0100

    Removed.

--- workflow/scripts/run_needle.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-genomic_sequence="${1}"
-ctcf_sequence="${2}"
-
-needle -asequence <(echo "${genomic_sequence}") \
-       -bsequence <(echo "${ctcf_sequence}") \
-       -gapopen 10 -gapextend 0.5 \
-       -outfile /dev/stdout
