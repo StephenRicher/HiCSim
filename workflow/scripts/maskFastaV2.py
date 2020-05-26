3f504fcd758af8b6f840f56114e714c57f7e41ff
Stephen Richer <sr467@bath.ac.uk>
2020-05-26 12:21:29 +0100

    Script to squeeze and generate mask genome.

---
+++ workflow/scripts/maskFastaV2.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+""" Mask a genome with relevant bases """
+
+
+import sys
+import random
+import pyCommonTools as pct
+from utilities import commaPair
+from collections import defaultdict
+
+
+def main():
+
+    __version__ = '1.0.0'
+
+    parser = pct.make_parser(verbose=True, version=__version__,)
+    parser.set_defaults(function=mask)
+    parser.add_argument(
+        '--chromSizes', required=True,
+        help='Chromosome sizes file.')
+    parser.add_argument(
+        '--bed', metavar='BED,CHAR', default=[],
+        type=commaPair, action='append',
+        help='BED file of regions to mask, paired with masking character.'
+        'Call multiple times to add more files.')
+
+    return (pct.execute(parser))
+
+
+def mask(chromSizes, bed):
+    regions = processBed(bed)
+    chromosomes = readChromosomes(chromSizes)
+    writeMaskedFasta(regions, chromosomes)
+
+
+def readChromosomes(chrom_sizes):
+    chromosomes = {}
+    with open(chrom_sizes) as fh:
+        for line in fh:
+            name, length = line.split()
+            chromosomes[name] = int(length)
+    return chromosomes
+
+
+def processBed(beds):
+    regions = defaultdict(lambda: defaultdict(list))
+    for bed, mask in beds:
+        with open(bed) as fh:
+            for line in fh:
+                entries = line.split()
+                ref = entries[0]
+                start = int(entries[1]) + 1 # convert to 1-based
+                end = int(entries[2])
+                mid = round((start + end)/2)
+                regions[ref][mid].append(mask)
+    return regions
+
+
+def writeMaskedFasta(regions, chromosomes):
+    for chromosome, length in chromosomes.items():
+        sys.stderr.write(f'>{chromosome}\n')
+        sys.stdout.write(f'>{chromosome}\n')
+        for index in range(1, length + 1):
+            if index in regions[chromosome]:
+                base = random.choice(regions[chromosome][index])
+            else:
+                base = 'N'
+            sys.stdout.write(base)
+        sys.stdout.write('\n')
+
+
+if __name__ == '__main__':
+    sys.exit(main())
