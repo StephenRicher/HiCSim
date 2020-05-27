252cb8b401084d896ed5f8ce90b5f67ff556c38b
Stephen Richer <sr467@bath.ac.uk>
2020-05-27 12:39:34 +0100

    Update bead to randomly select from beads that are proportional.

--- workflow/scripts/FastaToBeads.py
+++ workflow/scripts/FastaToBeads.py
@@ -3,76 +3,60 @@
 """ Script to read a single record FASTA file and compress N bases to 1 bead """
 
 import sys
-import argparse
+import random
 import pyCommonTools as pct
-from collections import Counter
-from timeit import default_timer as timer
 
 
 def main():
 
     __version__ = '1.0.0'
 
-    parser = pct.make_parser(
-        verbose=True, version=__version__, infile=True, in_type='FASTA',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser = pct.make_parser(version=__version__)
+    parser.add_argument('fasta', metavar='FASTA', help='Input FASTA file')
     parser.add_argument(
-        '-n', '--nbases', default=100, type=int,
+        '--nbases', default=1000, type=int,
         help='Number of base to represent 1 bead.')
     parser.set_defaults(function=compress)
 
     return (pct.execute(parser))
 
 
-def compress(infile, nbases):
+def compress(fasta, nbases):
 
     log = pct.create_logger()
-
-    with pct.open(infile) as f:
-
-        bases = Counter()
-        i = 0
-        for line_number, line in enumerate(f):
+    with pct.open(fasta) as fh:
+        bases = []
+        for i, line in enumerate(fh):
             if line.startswith('>'):
-                if line_number != 0:
+                if i != 0:
                     log.warning('Second FASTA record detected - skipping.')
-                    sys.exit(0)
+                    break
             else:
-                if line_number == 0:
+                if i == 0:
                     log.error('FASTA does not begin with ">".')
                     sys.exit(1)
                 else:
-                    line = line.strip()
-                    for base in line:
-                        i += 1
-                        if base != 'N':
-                            bases.update(base)
-                        if i == nbases:
+                    for base in line.strip():
+                        bases.append(base)
+                        if len(bases) == nbases:
                             sys.stdout.write(f'{get_bead(bases)}\n')
-                            bases = Counter()
-                            i = 0
-
-        if i > 0:
+                            bases = []
+        if len(bases) > 0:
             sys.stdout.write(f'{get_bead(bases)}\n')
 
 
 def get_bead(bases):
-    # If both CTCF orientations exist create a new bead 'B' to represent both
-    if 'F' in bases and 'R' in bases:
-        bases['B'] = bases['F'] + bases['R']
-        bases['F'] = 0
-        bases['R'] = 0
-    if sum(bases.values()) != 0:
-        bead = bases.most_common(1)[0][0]
-    else:
+    # Remove any 'N' bases from list
+    bases = [base for base in bases if base != 'N']
+    if len(bases) == 0:
         bead = 'N'
+    else:
+        # If both CTCF orientations exist create a bead 'B' to represent both
+        if 'F' in bases and 'R' in bases:
+            bases = ['B' if base in ['F', 'R'] else base for base in bases]
+        bead = random.choice(bases)
     return bead
 
 
 if __name__ == '__main__':
-    log = pct.create_logger()
-    start = timer()
-    RC = main()
-    end = timer()
-    log.info(f'Total time elapsed: {end - start} seconds.')
-    sys.exit(RC)
+    sys.exit(main())
