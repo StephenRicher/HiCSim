fa27ff8b1654971db8e6045e32efd021d280b326
Stephen Richer <sr467@bath.ac.uk>
2020-09-06 13:15:25 +0100

    Add center and remove vMin, vMax.

--- workflow/scripts/plotTUCorrelation.py
+++ workflow/scripts/plotTUCorrelation.py
@@ -27,8 +27,8 @@ def main(files: List, out: str, **kwargs) -> None:
 
     # Flip vertically to ensure diagonal goes from bottom left to top right
     correlation = correlation.iloc[::-1]
-
-    ax = sns.heatmap(correlation, cmap='bwr', vmin=-0.3, vmax=0.3)
+    #vmin=-0.3, vmax=0.3
+    ax = sns.heatmap(correlation, cmap='bwr', center=0)
     plt.savefig(out)
 
 
