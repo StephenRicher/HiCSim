ac4ea0b4d2b7edc7a1961c3a3dc2cfaa89bfe478
Stephen Richer <sr467@bath.ac.uk>
2020-09-07 16:43:34 +0100

    Modify background and bead colour and ensure full molecule is loaded before continuing.

--- workflow/scripts/vmd.tcl
+++ workflow/scripts/vmd.tcl
@@ -1,11 +1,20 @@
 set infile [lindex $argv 0]
 set gifdir [lindex $argv 1]
 
-mol new ${infile} type {xyz} first 0 last -1 step 1 waitfor 1
-mol modcolor 0 0 Element
+# Load XYZ file and wait until completely loaded
+mol new ${infile} type {xyz} first 0 last -1 step 1 waitfor -1
 mol modstyle 0 0 VDW 1.000000 12.000000
 mol modmaterial 0 0 Opaque
 
+# Colour by Element (1st column of XYZ file)
+mol modcolor 0 0 Element
+# Element 'H' (1) represents standard 'DNA' bead - change from white to blue
+color Element H blue
+# Element 'N' was blue so need to replace - change from blue to black
+color Element N black
+color Display Background white
+
+# Create directory to store image renderings
 file mkdir ${gifdir}
 
 set numframes [molinfo top get numframes]
@@ -16,7 +25,7 @@ set x_inc 1
 set y_inc 1
 set z_inc 1
 
-for {set i 0} {$i < $numframes} {incr i} {
+for {set i 0} {$i < $numframes} {incr i 50} {
   animate goto $i
   set filename ${gifdir}/snap.[format "%04d" $frame].tga
   render TachyonInternal ${filename}
