c36f94e0d4d5853300e28cd4ebed4ab289f29fe9
Stephen Richer <sr467@bath.ac.uk>
2020-09-10 21:17:10 +0100

    Update to new template and remove pyCommonTools.

--- workflow/scripts/npz2homer.py
+++ workflow/scripts/npz2homer.py
@@ -3,20 +3,33 @@
 """ Write HiC matrix to Homer format """
 
 import sys
+import logging
+import argparse
 import pandas as pd
 from utilities import npz
-import pyCommonTools as pct
 from scipy.sparse import load_npz
 
-def main():
 
-    __version__ = '1.0.0'
+__version__ = '1.0.0'
 
-    parser = pct.make_parser(verbose=True, version=__version__)
-    parser.set_defaults(function=npz2homer)
 
-    parser.add_argument('matrix', type=npz)
-    requiredNamed = parser.add_argument_group(
+def main(matrix, binsize, chromosome, start,  **kwargs) -> None:
+
+    matrix = load_npz(matrix).toarray()
+    names = [f'{chromosome}-{start+(i*binsize)}' for i in range(len(matrix))]
+    matrix = pd.DataFrame(matrix, columns=names)
+    matrix.insert(0, 'Regions', names)
+    matrix.insert(0, 'HiCMatrix (directory=.)', names)
+    matrix.to_csv(sys.stdout, sep='\t', index=False)
+
+
+def parse_arguments():
+
+    custom = argparse.ArgumentParser(add_help=False)
+    custom.set_defaults(function=main)
+    custom.add_argument('matrix', type=npz,
+        help='Contact matrix in .npz format.')
+    requiredNamed = custom.add_argument_group(
         'required named arguments')
     requiredNamed.add_argument(
         '--binsize', type=int, required=True,
@@ -27,19 +40,27 @@ def main():
     requiredNamed.add_argument(
         '--start', type=int, required=True,
         help='Start coordinate of matrix.')
+    epilog='Stephen Richer, University of Bath, Bath, UK (sr467@bath.ac.uk)'
 
-    return (pct.execute(parser))
+    base = argparse.ArgumentParser(add_help=False)
+    base.add_argument(
+        '--version', action='version', version=f'%(prog)s {__version__}')
+    base.add_argument(
+        '--verbose', action='store_const', const=logging.DEBUG,
+        default=logging.INFO, help='verbose logging for debugging')
 
+    parser = argparse.ArgumentParser(
+        epilog=epilog, description=__doc__, parents=[base, custom])
+    args = parser.parse_args()
 
-def npz2homer(matrix, binsize, chromosome, start) -> None:
+    log_format='%(asctime)s - %(levelname)s - %(funcName)s - %(message)s'
+    logging.basicConfig(level=args.verbose, format=log_format)
 
-    matrix = load_npz(matrix).toarray()
-    names = [f'{chromosome}-{start+(i*binsize)}' for i in range(len(matrix))]
-    matrix = pd.DataFrame(matrix, columns=names)
-    matrix.insert(0, 'Regions', names)
-    matrix.insert(0, 'HiCMatrix (directory=.)', names)
-    matrix.to_csv(sys.stdout, sep='\t', index=False)
+    return args
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+    args = parse_arguments()
+    return_code = args.function(**vars(args))
+    logging.shutdown()
+    sys.exit(return_code)
