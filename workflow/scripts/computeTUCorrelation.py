12a9559c05c18e328ea5b9a45ffeb5b330407f53
Stephen Richer <sr467@bath.ac.uk>
2020-09-16 11:03:27 +0100

    Remove ignoreZeroPair.

--- workflow/scripts/computeTUCorrelation.py
+++ workflow/scripts/computeTUCorrelation.py
@@ -39,40 +39,23 @@ def main(TuXYZ: str, TfXYZ: str, atomGroups: str, out: str, distance: float, ign
     atomGroupsDict = readJSON(atomGroups)
     TU_indexes = atomGroupsDict['TU']
 
-    # Compute correlation ignore when both pairs are 0
-    if ignoreZeroPair:
-        correlations = []
-        for idx1, col1 in enumerate(TU_indexes):
-            for idx2, col2 in enumerate(TU_indexes):
-                if col2 > col1: # Do not repeat duplicates
-                    col1NoZero = allDistances[~((allDistances[:,idx1]==.0) & (allDistances[:,idx2]==.0)),idx1]
-                    col2NoZero = allDistances[~((allDistances[:,idx1]==.0) & (allDistances[:,idx2]==.0)),idx2]
-                    # Ensure col1 & col2 both have atleast 1 True & False (TF)
-                    col1HaveTF = np.unique(col1NoZero).size == 2
-                    col2HaveTF = np.unique(col2NoZero).size == 2
-                    if col1HaveTF and col2HaveTF:
-                        cor = np.corrcoef(col1NoZero, col2NoZero)[-1,0]
-                        correlations.append([col1, col2, cor])
-        correlations = pd.DataFrame(
-            correlations, columns=['row', 'column', 'score'])
-    else:
-        # Convert to pandas so we can label columns with TU atom indexes
-        allDistances = pd.DataFrame(data=allDistances, columns=TU_indexes)
+    # Convert to pandas so we can label columns with TU atom indexes
+    allDistances = pd.DataFrame(data=allDistances, columns=TU_indexes)
 
-        # Compute Pearson correlation coefficient and convert to long format
-        correlations = allDistances.corr(method='pearson')
-        correlations = correlations.stack().reset_index()
-        correlations.columns = ['row', 'column', 'r']
+    # Compute Pearson correlation coefficient and convert to long format
+    correlations = allDistances.corr(method='pearson')
+    correlations = correlations.stack().reset_index()
+    correlations.columns = ['row', 'column', 'r']
 
-        # Combpute corresponding p-values and convert to seperate dataframe
-        pvalues = allDistances.corr(method=pearsonr_pval)
-        pvalues = pvalues.stack().reset_index()
-        pvalues.columns = ['row', 'column', 'p-value']
+    # Combpute corresponding p-values and convert to seperate dataframe
+    pvalues = allDistances.corr(method=pearsonr_pval)
+    pvalues = pvalues.stack().reset_index()
+    pvalues.columns = ['row', 'column', 'p-value']
 
-        # Combine to include p-values
-        pearson = pd.merge(correlations, pvalues)
+    # Combine to include p-values
+    allDistances = pd.merge(correlations, pvalues)
 
-    pearson.to_csv(out, index=False)
+    allDistances.to_csv(out, index=False)
 
 
 def pearsonr_pval(x,y):
@@ -104,10 +87,6 @@ def parse_arguments():
     custom.add_argument(
         '--out', default='TUcorrelation.csv',
         help='Contact matrix output (default: %(default)s)')
-    custom.add_argument(
-        '--ignoreZeroPair',  action='store_true',
-        help='Ignore timepoint where both TU pairs are inactive when '
-             'computing correlation. May be signicantly slower.')
     epilog='Stephen Richer, University of Bath, Bath, UK (sr467@bath.ac.uk)'
 
     base = argparse.ArgumentParser(add_help=False)
