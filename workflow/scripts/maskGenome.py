d7b9c933d9100f2bd02e89bcf6c65135b6a408a5
Stephen Richer <sr467@bath.ac.uk>
2020-10-02 12:40:28 +0100

    Remove pyCommonTools dependency.

--- workflow/scripts/maskGenome.py
+++ workflow/scripts/maskGenome.py
@@ -4,30 +4,49 @@
 
 import os
 import sys
-import pyCommonTools as pct
+import logging
+import argparse
+import fileinput
 
-def main():
+__version__ = '1.0.0'
 
-    __version__ = '1.0.0'
-
-    parser = pct.make_parser(version=__version__,)
-    parser.set_defaults(function=maskAllBases)
-    parser.add_argument(
-        'fasta', metavar='FASTA', help='Input FASTA file to mask.')
-
-    return (pct.execute(parser))
-
-
-def maskAllBases(fasta):
+def main(file, **kwargs):
     """ Replace all bases in genome sequence with N. """
 
-    with pct.open(fasta) as fh:
+    with fileinput.input(file) as fh:
         for line in fh:
             if line.startswith('>'):
                 sys.stdout.write(line)
             else:
                 sys.stdout.write(f'{"N"*len(line.strip())}\n')
 
+def parse_arguments():
+
+    custom = argparse.ArgumentParser(add_help=False)
+    custom.set_defaults(function=main)
+    custom.add_argument(
+        'file', metavar='FASTA', help='Input FASTA file to mask.')
+    epilog='Stephen Richer, University of Bath, Bath, UK (sr467@bath.ac.uk)'
+
+    base = argparse.ArgumentParser(add_help=False)
+    base.add_argument(
+        '--version', action='version', version=f'%(prog)s {__version__}')
+    base.add_argument(
+        '--verbose', action='store_const', const=logging.DEBUG,
+        default=logging.INFO, help='verbose logging for debugging')
+
+    parser = argparse.ArgumentParser(
+        epilog=epilog, description=__doc__, parents=[base, custom])
+    args = parser.parse_args()
+
+    log_format='%(asctime)s - %(levelname)s - %(funcName)s - %(message)s'
+    logging.basicConfig(level=args.verbose, format=log_format)
+
+    return args
+
 
 if __name__ == '__main__':
-    sys.exit(main())
+    args = parse_arguments()
+    return_code = args.function(**vars(args))
+    logging.shutdown()
+    sys.exit(return_code)
