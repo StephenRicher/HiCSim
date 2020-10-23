058d424cd063907eeb53c18927486934c9b94cda
Stephen Richer <sr467@bath.ac.uk>
2020-10-24 00:27:48 +0100

    Remove pairwise comparison.

--- workflow/scripts/processTUinfo.py
+++ workflow/scripts/processTUinfo.py
@@ -3,33 +3,27 @@
 """ Determine TU activation state at each time point """
 
 import sys
-import json
 import logging
 import argparse
 import numpy as np
 import pandas as pd
 from utilities import getAtomCount, readJSON
-from itertools import combinations
-from scipy.spatial.distance import cdist, pdist
+from scipy.spatial.distance import cdist
 
 
 __version__ = '1.0.0'
 
-def main(file: str, atomGroups: str, distance: float, outTU: str, outPairDistance: str, **kwargs) -> None:
+def main(file: str, atomGroups: str, distance: float, out: str, **kwargs) -> None:
 
     # Read atomGroup and retrieve TU atom indexes
     atomGroupsDict = readJSON(atomGroups)
-    names1, names2 = generatePairwiseNames(atomGroupsDict['TU'])
-
     atomCount = getAtomCount(atomGroupsDict)
 
     # Read simulation file 1 timestep at a time
-    info = pd.read_csv(file, usecols=['time', 'id', 'type', 'x', 'y', 'z'],
-        chunksize=atomCount)
+    info = pd.read_csv(file,
+        usecols=['time', 'id', 'type', 'x', 'y', 'z'], chunksize=atomCount)
 
     allTUinfo = []
-    allTUpairs = []
-
     for sim in info:
 
         # Extract data of only TUs
@@ -48,39 +42,7 @@ def main(file: str, atomGroups: str, distance: float, outTU: str, outPairDistanc
         TUdata['active'] = TUdata['TFnear'] < distance
         allTUinfo.append(TUdata)
 
-        activeTUpos = []
-        inactiveTUpos = []
-        activeTUs = TUdata['active'].to_list()
-        for i, TU in enumerate(TUs):
-            if activeTUs[i]:
-                activeTUpos.append(TU)
-                inactiveTUpos.append([np.nan, np.nan, np.nan])
-            else:
-                activeTUpos.append([np.nan, np.nan, np.nan])
-                inactiveTUpos.append(TU)
-
-        allTUpairs.append(pd.DataFrame(
-            {'TU1'      : names1                         ,
-             'TU2'      : names2                         ,
-             'active'   : pdist(activeTUpos,   'euclidean'),
-             'inactive' : pdist(inactiveTUpos, 'euclidean')}))
-
-    # Average active TU-TU pair distances across timepoints and save
-    allTUpairs = pd.concat(allTUpairs)
-    allTUpairs = allTUpairs.groupby(['TU1', 'TU2']).mean().reset_index()
-    allTUpairs.to_csv(outPairDistance, index=False)
-
-    pd.concat(allTUinfo).to_csv(outTU, index=False)
-
-
-def generatePairwiseNames(names):
-    names1 = []
-    names2 = []
-    names = combinations(names, 2)
-    for name1, name2 in names:
-        names1.append(name1)
-        names2.append(name2)
-    return names1, names2
+    pd.concat(allTUinfo).to_csv(out, index=False)
 
 
 def parse_arguments():
@@ -97,10 +59,7 @@ def parse_arguments():
         '--distance', default=1.8, type=float,
         help='Distance threshold for active transcription (default: %(default)s)')
     custom.add_argument(
-        '--outPairDistance', default='TU-pairDistance.csv.gz',
-        help='File to write active TU pair distances (default: %(default)s)')
-    custom.add_argument(
-        '--outTU', default='TU-info.csv.gz', help='File to TU info.')
+        '--out', default=sys.stdout, help='File to TU info (default: stdout)')
     epilog='Stephen Richer, University of Bath, Bath, UK (sr467@bath.ac.uk)'
 
     base = argparse.ArgumentParser(add_help=False)
