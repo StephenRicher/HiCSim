d039e801a25593b8c4afb3ae59e103ff0fad0fa2
Stephen Richer <sr467@bath.ac.uk>
2020-11-10 11:29:13 +0000

    Upsate parseArgs().

--- workflow/scripts/custom2XYZ.py
+++ workflow/scripts/custom2XYZ.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python3
 
-""" Convert custom LAMMPS dump output to XYZ format """
+""" Convert custom LAMMPS output to XYZ format """
 
-import os
-import re
 import sys
-import logging
 import argparse
 import fileinput
-import collections
+from utilities import setDefaults
 
 __version__ = '1.0.0'
 
 
-def main(file, **kwargs):
+def custom2XYZ(file: str):
 
     with fileinput.input(file) as fh:
         for line in fh:
@@ -21,20 +18,18 @@ def main(file, **kwargs):
             if not line:
                 continue
             if timeStepHeader(line):
-                timeStep = int(next(fh).strip())
+                timeStep = next(fh).strip()
             elif nAtomHeader(line):
-                nAtoms = int(next(fh).strip())
+                nAtoms = next(fh).strip()
             elif coordinateHeader(line):
                 print(f'{nAtoms}\nAtoms. Timestep: {timeStep}')
                 for line in fh:
-                    try:
-                        id, type, x, y, z, ix, iy, iz = line.strip().split()
-                        print(type, x, y, z)
-                    # Continue reading coordinates until next timeStepHeader
-                    except ValueError:
-                        timeStep = int(next(fh).strip())
+                    if timeStepHeader(line):
+                        timeStep = next(fh).strip()
                         break
-
+                    else:
+                        id, type, x, y, z = line.split()[:5]
+                        print(type, x, y, z)
 
 
 def timeStepHeader(line):
@@ -49,34 +44,17 @@ def nAtomHeader(line):
     return line.startswith('ITEM: NUMBER OF ATOMS')
 
 
-def parse_arguments():
+def parseArgs():
 
-    custom = argparse.ArgumentParser(add_help=False)
-    custom.set_defaults(function=main)
-    custom.add_argument(
+    epilog = 'Stephen Richer, University of Bath, Bath, UK (sr467@bath.ac.uk)'
+    parser = argparse.ArgumentParser(epilog=epilog, description=__doc__)
+    parser.add_argument(
         'file', metavar='FILE', nargs='?', default=[],
-        help='Lammps custom dump outputRemoteTowe (default: stdin)')
-    epilog='Stephen Richer, University of Bath, Bath, UK (sr467@bath.ac.uk)'
+        help='Lammps custom output (default: stdin)')
 
-    base = argparse.ArgumentParser(add_help=False)
-    base.add_argument(
-        '--version', action='version', version=f'%(prog)s {__version__}')
-    base.add_argument(
-        '--verbose', action='store_const', const=logging.DEBUG,
-        default=logging.INFO, help='verbose logging for debugging')
-
-    parser = argparse.ArgumentParser(
-        epilog=epilog, description=__doc__, parents=[base, custom])
-    args = parser.parse_args()
-
-    log_format='%(asctime)s - %(levelname)s - %(funcName)s - %(message)s'
-    logging.basicConfig(level=args.verbose, format=log_format)
-
-    return args
+    return setDefaults(parser, verbose=False, version=__version__)
 
 
 if __name__ == '__main__':
-    args = parse_arguments()
-    return_code = args.function(**vars(args))
-    logging.shutdown()
-    sys.exit(return_code)
+    args = parseArgs()
+    sys.exit(custom2XYZ(**vars(args)))
