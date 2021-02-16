4f3c35397fa3672e0503c9a687f203363f13e831
Stephen Richer <sr467@bath.ac.uk>
2021-02-16 10:39:09 +0000

    Ensure timestep is integer.

--- workflow/scripts/runLammpsSimulation.py
+++ workflow/scripts/runLammpsSimulation.py
@@ -73,7 +73,7 @@ def runLammps(equil: str, atomGroups: str, simTime: int, TADStatus: str,
     nSteps = 10
     for bond in np.linspace(initialFeneLength, maxFeneLength, nSteps):
         lmp.command(f'bond_coeff 1 fene 30 {bond} 1.0 1.0')
-        lmp.command(f'run {equilTime /  nSteps}')
+        lmp.command(f'run {int(equilTime /  nSteps)}')
 
     if pairCoeffs:
         writePairCoeffs(pairCoeffs, beadTypes)
